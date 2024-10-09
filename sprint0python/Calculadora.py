7c3eb646f3fb850ae4cd1ed4968fc21ac7e151e5
Fluzze7 <jpregof23@fpcoruna.afundacion.org>
2024-10-09 10:05:51 +0200

    Calculadora.py con comentarios

--- sprint0python/Calculadora.py
+++ sprint0python/Calculadora.py
@@ -1,29 +1,26 @@
 from operaciones import *
-
-while  True:
+while  True: #función que solicita por teclado la variable n1 y solo termina cuando este es un numero
     n1 = input("N1:   ")
-    if compnum(n1):
-        n1 = pasaranum(n1)
+    if checknum(n1):
+        n1 = tonum(n1)
         break
-while  True:
+while  True: #misma función que la anterior pero para la variable n2
     n2 = input("N2:   ")
-    if compnum(n2):
-        n2 = pasaranum(n2)
+    if checknum(n2):
+        n2 = tonum(n2)
         break
-print("Ambos numeros son: n1: ",n1,"y n2: ",n2)
-
+print("Ambos numeros son: n1: ",n1,"y n2: ",n2) #muestra por consola los valores introducidos
+# por el usuario en las variables n1 y n2.
 print("Que operación deseas hacer? (+-*/)")
-operacion = input()
+operacion = input() #lee un valor por teclado esperando recibir +-*/, en caso de no hacerlo no haría nada
+#sino, en funcion del simbolo hace dicha operación.
 if operacion == "+":
-    print("Resultado de la suma = ",suma(n1,n2))
+    print("Resultado de la suma = ",add(n1,n2))
 elif operacion == "-":
-    print("Resultado de la resta = " , resta(n1,n2))
+    print("Resultado de la resta = " , substract(n1,n2))
 elif operacion == "-":
-    print("Resultado de la multiplicación = " , multiplicacion(n1,n2))
+    print("Resultado de la multiplicación = " , product(n1,n2))
 elif operacion == "/":
-    if n2 == 0:
-        print("NO SE PUEDE DIVIDIR ENTRE 0")
-    else:
-        print("Resultado de la división = ", dividision(n1,n2) )
+        print("Resultado de la división = ", division(n1,n2) )
 else:
     print("No has introducido una opción valida.")
