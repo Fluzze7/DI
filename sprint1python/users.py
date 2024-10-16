749f64d931a5ff1a882285007b55c4f869c6d3f3
Fluzze7 <jpregof23@fpcoruna.afundacion.org>
2024-10-16 10:31:19 +0200

    Borrando subida por error de todo el repositorio

--- sprint1python/users.py
+++
@@ -1,8 +0,0 @@
-class User:
-    def __init__(self,name,age,genre):
-        self.name = name
-        self.age = age
-        self.genre = genre
-
-    def to_str(self):
-        return f"{self.name} ,{self.age} ,{self.genre}"
